4cfcd11cc3e9b8213f92fa2a55576e8cb6ba57c2
Willian-Berrocal <45219580+Willian-Berrocal@users.noreply.github.com>
2019-10-24 19:00:53 -0500

    logout

--- code/WebProject/WebProject/WebProject/views.py
+++ code/WebProject/WebProject/WebProject/views.py
@@ -62,7 +62,7 @@ def about():
 def login():                     
     """Renders login """
     return render_template(
-        'login_final.html',
+        'login.html',
         title='Login',
         year=datetime.now().year,
         message='Login to your existing account'
@@ -143,3 +143,9 @@ def current_user():
     db_session = db.getSession(engine)
     user = db_session.query(entities.User).filter(entities.User.id == session['logged_user']).first()
     return Response(json.dumps(user,cls=connector.AlchemyEncoder),mimetype='application/json')
+
+
+@app.route('/logout', methods = ['GET'])
+def logout():
+    session.clear()
+    return render_template('index.html')    
